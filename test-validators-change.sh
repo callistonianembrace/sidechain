625f58bb0af97bb0b9e705ee0545e9e24db66ae8
callistonianembrace <87455417+callistonianembrace@users.noreply.github.com>
2021-11-19 19:02:54 +0530

    More tests

--- test-validators-change.sh
+++ test-validators-change.sh
@@ -1,6 +1,5 @@
  #! /bin/sh
 
-
 FOLDER=4
 
 SIDECLI=$(esy x which sidecli)
@@ -9,10 +8,16 @@ sidecli () {
 }
 
 ADDRESS=$(sidecli self "$FOLDER" | grep "address:" | awk '{ print $2 }')
+CURRENT_BLOCK_PRODUCER=2
 
 sidecli add-trusted-validator 0 ${ADDRESS}
 sidecli add-trusted-validator 1 ${ADDRESS}
 sidecli add-trusted-validator 2 ${ADDRESS}
-sidecli propose-new-validator  0 ${ADDRESS}
-sidecli register-uri 0 $(jq -r .secret 4/identity.json) uri 
-esy x deku-node "$FOLDER"
+sidecli propose-new-validator ${CURRENT_BLOCK_PRODUCER} ${ADDRESS}
+read -p "Proceed with removal?" foo
+# sidecli register-uri 0 $(jq -r .secret 4/identity.json) $(jq -r .uri 4/identity.json)  
+sidecli remove-trusted-validator 0 ${ADDRESS}
+sidecli remove-trusted-validator 1 ${ADDRESS}
+sidecli remove-trusted-validator 2 ${ADDRESS}
+sidecli propose-validator-removal ${CURRENT_BLOCK_PRODUCER} ${ADDRESS}
+# esy x deku-node "$FOLDER"
