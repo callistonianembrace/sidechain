3fb904366e696980c444c06e5d195c5ebfb7697e
callistonianembrace <87455417+callistonianembrace@users.noreply.github.com>
2021-11-19 13:24:16 +0530

    wip: test script fix: trust on all nodes

--- test-validators-change.sh
+++ test-validators-change.sh
@@ -10,8 +10,9 @@ sidecli () {
 
 ADDRESS=$(sidecli self "$FOLDER" | grep "address:" | awk '{ print $2 }')
 
-# esy x deku-node "$FOLDER" 
-# sidecli add-trusted-validator 0 ${ADDRESS}
-# sidecli propose-new-validator  0 ${ADDRESS}
-# sidecli register-uri 0 $(jq -r .secret 4/identity.json) uri 
-# esy x deku-node "$FOLDER"
+sidecli add-trusted-validator 0 ${ADDRESS}
+sidecli add-trusted-validator 1 ${ADDRESS}
+sidecli add-trusted-validator 2 ${ADDRESS}
+sidecli propose-new-validator  0 ${ADDRESS}
+sidecli register-uri 0 $(jq -r .secret 4/identity.json) uri 
+esy x deku-node "$FOLDER"
